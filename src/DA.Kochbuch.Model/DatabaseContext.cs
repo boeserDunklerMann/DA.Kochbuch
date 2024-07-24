2889677aff010d6b8a05bb9e7d66578a3fdb42e4
André <andre.dunkel@t-online.de>
2024-07-24 12:46:27 +0200

    DatabaseContext created

---
+++ src/DA.Kochbuch.Model/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DA.Kochbuch.Model
+{
+	/// <ChangeLog>
+	/// <Create Datum="24.07.2024" Entwickler="DA" />
+	/// </ChangeLog>
+	public class DatabaseContext : DbContext
+	{
+		public DatabaseContext() { }
+		public System.Data.Entity.IDbSet<Recipe>  Recipes { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// TODO: see here: https://github.com/entityframeworktutorial/EF6-Code-First-Demo/blob/master/EF6CodeFirstDemo/SchoolContext.cs
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
