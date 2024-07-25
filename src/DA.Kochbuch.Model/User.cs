f65f546802a8ea7fdceb10a582c0e62e6c36e465
André <andre.dunkel@t-online.de>
2024-07-25 17:20:40 +0200

    class User created
    EFsupport added

---
+++ src/DA.Kochbuch.Model/User.cs
@@ -0,0 +1,15 @@
+﻿namespace DA.Kochbuch.Model
+{
+	/// <ChangeLog>
+	/// <Create Datum="25.07.2024" Entwickler="DA" />
+	/// </ChangeLog>
+	public class User : BaseModel
+	{
+		public ICollection<Recipe> Recipes { get; set; }
+
+		public override void PopulateMyID()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
