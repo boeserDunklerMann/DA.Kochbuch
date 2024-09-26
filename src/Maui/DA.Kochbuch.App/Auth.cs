73739ae91c2cdb66fcae2e1f4d7b83fcf475e812
André <andre.dunkel@t-online.de>
2024-09-26 14:05:31 +0200

    some Google-API-stuff

---
+++ src/Maui/DA.Kochbuch.App/Auth.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace DA.Kochbuch.App
+{
+	/// <ChangeLog>
+	/// <Create Datum="26.09.2024" Entwickler="DA" />
+	/// </ChangeLog>
+	/// <remarks>
+	/// https://github.com/JayMalli/Google_OAuth2.0-MAUI
+	/// https://dev.to/jaymalli_programmer/google-oauth-20-authorization-service-implementation-in-net-maui-okl
+	/// </remarks>
+	public static class Auth
+	{
+		public static string ConstructGoogleSignInUrl()
+		{
+			// Specify the necessary parameters for the Google Sign-In URL
+			const string clientId = "371013138451-6uc53r25qa6mgjm98sea4rp25p3eovum.apps.googleusercontent.com";
+
+			const string responseType = "code";
+			const string accessType = "offline";
+			const string redirect_uri = "http://localhost:8080";
+			const string scope = "https://www.googleapis.com/auth/drive";
+
+			// Construct the Google Sign-In URL
+			return "https://accounts.google.com/o/oauth2/v2/auth" +
+							$"?client_id={Uri.EscapeDataString(clientId)}" +
+							$"&redirect_uri={Uri.EscapeDataString(redirect_uri)}" +
+							$"&response_type={Uri.EscapeDataString(responseType)}" +
+							$"&scope={Uri.EscapeDataString(scope)}" +
+							$"&access_type={Uri.EscapeDataString(accessType)}" +
+							"&include_granted_scopes=true" +
+							"&prompt=consent";
+		}
+
+		public static string? OnWebViewNavigating(WebNavigatingEventArgs e, ContentPage signInContentPage)
+		{
+			if (e.Url.StartsWith("http://localhost"))
+			{
+				Uri uri = new Uri(e.Url);
+				string query = WebUtility.UrlDecode(uri.Query);
+				var queryParams = System.Web.HttpUtility.ParseQueryString(query);
+				string authorizationCode = queryParams.Get("code");
+
+				signInContentPage.Navigation.PopModalAsync();
+				return authorizationCode;
+			}
+			return null;
+		}
+		public static (string?, string?) ExchangeCodeForAccessToken(string code)
+		{
+			// Configure the necessary parameters for the token exchange
+			const string clientId = "371013138451-6uc53r25qa6mgjm98sea4rp25p3eovum.apps.googleusercontent.com";
+			const string clientSecret = "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";
+			const string redirectUri = "http://localhost:8080";
+
+			// Create an instance of HttpClient
+			using (HttpClient client = new HttpClient())
+			{
+				// Construct the token exchange URL
+				const string tokenUrl = "https://oauth2.googleapis.com/token";
+
+				// Create a FormUrlEncodedContent object with the required parameters
+				FormUrlEncodedContent content = new FormUrlEncodedContent(new Dictionary<string, string>
+			  {
+				   { "code", code },
+				   { "client_id", clientId },
+				   { "client_secret", clientSecret },
+				   { "redirect_uri", redirectUri },
+				   { "grant_type", "authorization_code" }
+			  });
+
+				// Send a POST request to the token endpoint to exchange the code for an access token
+				HttpResponseMessage response = client.PostAsync(tokenUrl, content).Result;
+
+				// Check if the request was successful
+				if (response.IsSuccessStatusCode)
+				{
+					// Read the response content
+					string responseContent = response.Content.ReadAsStringAsync().Result;
+
+					// Parse the JSON response to extract the access token
+					JObject json = JObject.Parse(responseContent);
+					string accessToken = json.GetValue("access_token").ToString();
+					string refreshToken = json.GetValue("refresh_token").ToString();
+					return (accessToken, refreshToken);
+				}
+
+				else
+				{
+					// Exception:  "Token exchange request failed with status code: {response.StatusCode}"
+				}
+			}
+
+			return (null, null);
+		}
+		public static async void GetUsersDetailsAsync(string accessToken)
+		{
+			string url = $"https://www.googleapis.com/oauth2/v3/userinfo?access_token={accessToken}";
+			using(HttpClient client = new HttpClient())
+			{
+				HttpResponseMessage response = await client.GetAsync(url);
+				if (response.IsSuccessStatusCode)
+				{
+					JObject jsonObj = JObject.Parse(await response.Content.ReadAsStringAsync());
+					if (jsonObj != null)
+					{
+
+					}
+				}
+			}
+		}
+	}
+}
--- src/Maui/DA.Kochbuch.App/Pages/MainPage.xaml.cs
+++ src/Maui/DA.Kochbuch.App/Pages/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 
 namespace DA.Kochbuch.App
 {
+	/// <ChangeLog>
+	/// <Create Datum="??.09.2024" Entwickler="DA" />
+	/// <Change Datum="26.09.2024" Entwickler="DA">btnLogin_Clicked added</Change>
+	/// </ChangeLog>
 	public partial class MainPage : ContentPage
 	{
 		public MainPage()
@@ -18,6 +22,45 @@ namespace DA.Kochbuch.App
 		private async void ContentPage_Loaded(object sender, EventArgs e)
 		{
 			await ((MainVM)(BindingContext)).LoadDataAsync();
+		}
+
+		private void btnLogin_Clicked(object sender, EventArgs e)
+		{
+			WebView signInWebView = new WebView
+			{
+				Source = Auth.ConstructGoogleSignInUrl(),
+				VerticalOptions = LayoutOptions.Fill
+			};
+			// Dirty workaround: "google in mobile apps doesn't allow auth2.0 authentication via webview for security reasons":
+			// https://stackoverflow.com/a/69342626/12445867
+			signInWebView.UserAgent = $"Mozilla/5.0 (Linux; Android {DeviceInfo.Current.Version.Major}.{DeviceInfo.Current.Version.Minor}; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36";
+			
+			Grid grid = new Grid();
+			grid.RowDefinitions.Add(new RowDefinition { Height= GridLength.Star }); // row for webview
+			grid.SetRow(signInWebView, 0);
+			grid.Children.Add(signInWebView);
+			ContentPage signInContentPage = new ContentPage
+			{
+				Content = grid,
+			};
+			Application.Current.MainPage.Navigation.PushModalAsync(signInContentPage);
+			signInWebView.Navigating += (sender, e) =>
+			{
+				string? code = Auth.OnWebViewNavigating(e, signInContentPage);
+				if (e.Url.StartsWith("http://localhost") && code != null)
+				{
+					Console.WriteLine($"code: {code}");
+					(string access_token, string refresh_token) = Auth.ExchangeCodeForAccessToken(code);
+					if (access_token != null && refresh_token != null)
+					{
+						Console.WriteLine($"access_token:  {access_token}");
+						Console.WriteLine($"refresh_token: {refresh_token}");
+						Auth.GetUsersDetailsAsync(code);
+						Auth.GetUsersDetailsAsync(access_token);
+						Auth.GetUsersDetailsAsync(refresh_token);
+					}
+				}
+			};
 		}
 	}
 }
